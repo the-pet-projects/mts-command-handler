b07007c3c27ef6a38e7818b865948e3f39c46c22
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-12-01 13:46:48 +0000

    basic structure with docker compose and deploy folder

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mts.CommandHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
