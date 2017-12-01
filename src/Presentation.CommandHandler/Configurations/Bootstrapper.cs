899af09a71d12ef645603440cbbcd6ea286ccba4
Pedro Moreira <pedromsmoreira51@gmail.com>
2017-12-01 15:05:10 +0000

    add initial bootstrapper setup for logs and consul

---
+++ src/Presentation.CommandHandler/Configurations/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿namespace PetProjects.Mts.CommandHandler.Presentation.ConsoleApplication.Configurations
+{
+    using System.IO;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            this.SetupConfiguration();
+        }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public IServiceCollection ServiceCollection { get; private set; }
+
+        private void SetupConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .AddEnvironmentVariables("MTS_APP_SETTINGS");
+
+            this.Configuration = configurationBuilder.Build();
+        }
+
+        public Bootstrapper BootstrapContainer()
+        {
+            this.ServiceCollection = new ServiceCollection();
+            
+            // Setup Services, repositories, Producers, consumers, etc
+
+            return this;
+        }
+    }
+}
---
+++ src/Presentation.CommandHandler/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PetProjects.Mts.CommandHandler.Presentation.ConsoleApplication
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Configurations;
+    using Framework.Logging.Consumer.ElasticSearch;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    internal class Program
+    {
+        private static readonly ManualResetEvent QuitEvent = new ManualResetEvent(false);
+
+        private static void Main(string[] args)
+        {
+            // Init Bootstrapper with appsettings
+            var configuration = new Bootstrapper()
+                                .BootstrapContainer();
+
+            using (var parentServiceProvider =  configuration.ServiceCollection.BuildServiceProvider())
+            {
+                Program.Run(parentServiceProvider);
+            }
+        }
+
+        private static void Run(IServiceProvider scopedProvider)
+        {
+            var logger = scopedProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+
+            logger.LogCritical("Starting Mts CommandHandler...");
+
+            scopedProvider.StartPetProjectElasticLogConsumer();
+
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                Program.QuitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            Program.QuitEvent.WaitOne();
+
+            logger.LogCritical("Mts.CommandHandler Ended...");
+
+            // wait 2 seconds for previous log to reach the sink
+            Task.Delay(TimeSpan.FromMilliseconds(2000)).Wait();
+        }
+    }
+}
